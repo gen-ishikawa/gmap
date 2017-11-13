63773224908d3b68f05f5ab40e626a66bf6baf03
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-11-13 07:35:35 +0000

    いろいろ試し中

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Gmap
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.assets.paths << Rails.root.join("css", "img", "js", "mail", "scss", "vendor")
   end
 end
